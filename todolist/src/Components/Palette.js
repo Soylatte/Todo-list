94ae32248d8d3724f8160b71f2fb515f2c354a23
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-04 21:48:20 +0100

    add Component

--- todolist/src/Components/Palette.js
+++ todolist/src/Components/Palette.js
@@ -1,6 +1,26 @@
 import React from "react";
+import './Palette.css';
 
-const Palette = () => {
+const Color = ({color, active, onClick}) => {
+    return (
+        <div className={`color: ${active && 'active'}`} style={{background: color}} onClick={onClick}>
+
+        </div>
+    )
+}
+const Palette = ({colors, selected, onSelect}) => {
+    const colorList = colors.map(
+        (colors) => (<Color color={color} active={selected === color} onClick={() => onSelect(color)}
+        key={color}/>)
+        );
+    return(
+        <div className="palette">
+            {colorList}
+
+        </div>
+    )
+
+
 
 }
 
