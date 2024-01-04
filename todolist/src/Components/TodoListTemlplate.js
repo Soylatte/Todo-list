8ca9b5361f8bf7b479162c18f58677f1f9691190
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-04 21:32:13 +0100

    add Component

--- todolist/src/Components/TodoListTemlplate.js
+++ todolist/src/Components/TodoListTemlplate.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './TodoListTemplate.css';
 
-const TodoListTemplate = ({form,children}) => {
+const TodoListTemplate = ({form,children,palette}) => {
     return(
         <main className="todo-list-template">
             <div className="title">
                 Today's TodoList
 
+            </div>
+            <div className="palette">
+                {palette}
             </div>
             <section className="from-wrapper">
                 {form}
